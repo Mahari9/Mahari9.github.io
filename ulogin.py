68b69437afaf70c386d1cfab09b7a4c3b32b6241
Mahari Weldegergis <132389400+mahari9@users.noreply.github.com>
2024-07-16 16:19:17 +0300

    add back end flask

---
+++ ulogin.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+
+from models import storage
+from models.user import User
+from os import environ
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Flask
+from werkzeug.security import check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from sqlalchemy import or_
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and ('email' in request.form or 'phone' in request.form) and 'password' in request.form:
+        try:
+            # Query for filtering
+            user = storage.all(User).filter(or_(User.email == request.form['email'], User.phone == request.form['phone'])).first()
+            if user:
+                if check_password_hash(user.password, request.form['password']):
+                    flash('Logged in successfully!', category='success')
+                    login_user(user, remember=True)
+                    return redirect(url_for('templates.vehicle'))
+                else:
+                    flash('Incorrect password, try again.', category='error')
+            else:
+                flash('Email address not found.', category='warning')
+        except Exception as e:
+            flash(f"An error occurred: {str(e)}", category='error')
+        return render_template("login.html", user=current_user)
+
+    return render_template("login.html", user=current_user)  # Display login form for GET requests
+
+@login.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ uregister.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+
+from models import storage
+from models.user import User
+from os import environ
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Flask
+from werkzeug.security import generate_password_hash
+from flask_login import login_user, current_user
+from sqlalchemy import or_
+  
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+@app.route('/register', methods =['GET', 'POST'])
+def Register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        full_name = request.form.get('full_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = storage.all(User).filter(or_(User.email == "email",
+                                             User.phone == "phone")).first()
+        if user:
+            flash('Email/Phone already exists.', category='error')
+        elif len(email) < 4 or len(phone) != (10 or 15):
+            flash('Email/Phone must be > 3/10 or 15 chars', category='error')
+        elif len(full_name) < 5:
+            flash('Full_name must be greater than 4 char.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 4:
+            flash('Password must be at least 5 characters.', category='error')
+        else:
+            new_user = User(email=email, phone=phone, full_name=full_name,
+                            password=generate_password_hash(password1, method='sha256'))
+            storage.new(new_user)
+            storage.save()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('templates.login'))
+
+    return render_template("register.html", user=current_user)
+    
+if __name__ == "__main__":
+    """Register Function """
+    app.run(host='0.0.0.0', port=5000)
